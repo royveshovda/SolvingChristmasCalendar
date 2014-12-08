d564ef3ce205ccf135a7f8b07eefb9a0ce824f63
Roy Veshovda <roy@veshovda.no>
2014-12-08 06:05:22 +0100

    Fix for Sol 06

--- source/SolvingChristmasCalendar/Calendar06.fs
+++ source/SolvingChristmasCalendar/Calendar06.fs
@@ -35,6 +35,8 @@
 //var dN = numbers.Distinct();
 //int length = dN.ToList().Count;
 
+let union left right =
+    List.append left right |> Seq.distinct |> List.ofSeq
 
 let getOneArray limit factor =
     [1 .. limit] |> List.map (fun x -> x * factor)
@@ -43,12 +45,13 @@ let getOneArray limit factor =
 let getAllArrays limit =
     let d1 = [1 .. limit]
     let d2 = d1 |> List.map (getOneArray limit)
-    let d3 = d2 |> List.fold (fun acc x -> acc@x) []
-    let d4 = d3 |> List.toSeq
-    let d5 = d4 |> Seq.distinct
-    let d6 = d5 |> Seq.toList
-    let l1 = d6.Length
-    Seq.length d5
+    let d3 = d2 |> List.fold (fun acc x -> union acc x) []
+    //let d4 = d3 |> List.toSeq
+    //let d5 = d4 |> Seq.distinct
+    //let d6 = d5 |> Seq.toList
+    //let l1 = d6.Length
+    //Seq.length d5
+    d3.Length
 
 let getSolution =
     let d1 = getAllArrays 8000
