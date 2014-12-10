0b2e34e5a4302227b2421ec12f5cdea202650176
Roy Veshovda <roy@veshovda.no>
2014-12-10 06:53:38 +0100

    Defining Problem 10

--- source/SolvingChristmasCalendar/Calendar10.fs
+++ source/SolvingChristmasCalendar/Calendar10.fs
@@ -1,6 +1,13 @@
 ﻿module Calendar10
 
 //PROBLEM
-//Not opened yet
+//Velkommen til Knowit sitt julebord!
+//På julebordet er vi 1500 mennesker som sitter rundt et stort bord. Vi er nummerert fra 1 til 1500 langs bordet. Vi har fått tak i en flaske med meget sterk (men god!) akevitt.
+//Den første personen tar flasken og serverer person nummer to en dram. Akevitten er så sterk at denne personen går rett i bakken. Person nummer en sender så flaska videre til den tredje personen som serverer den fjerde en dram. Han går også rett i bakken og flasken sendes videre til femtemann. Dette fortsetter rundt slik at person nummer 1499 serverer person 1500 en dram (hvorpå han dundrer i gulvet) og gir flaska tilbake den første.
+//Nå serverer den første personen den tredje (som deiser av stolen) og gir flaska videre til den femte...
+//Dette fortsetter til det bare er en person igjen. Hvilken person sitter igjen ved julebordets slutt?
+//Svaret skal være i form av nummeret på personen som sitter igjen, eksempelvis 1337
+
+//Person: Number; Next Number
 
 let getSolution = "Not implemented yet"
