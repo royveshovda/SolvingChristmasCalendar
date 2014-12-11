f8d51c5f5786e2d2175ca86c3ccbb8a95cb1c240
Roy Veshovda <roy@veshovda.no>
2014-12-11 08:39:18 +0100

    Trying to solve 11

--- source/SolvingChristmasCalendar/Calendar11.fs
+++ source/SolvingChristmasCalendar/Calendar11.fs
@@ -15,11 +15,25 @@
 //Hva er N?
 
 open System
- 
+
+let is_sum (l: int64 list) (n: int64) =
+    (List.sum l) = n
+
+let get_candidates_of_length length (list: int64 list) =
+    let rec get_candidates_of_length_acc len (lst: int64 list) position acc =
+        let e = (position + len)
+        match e with
+        | x when x > lst.Length -> acc
+        | _ -> 
+            let add = lst |> List.toSeq |> Seq.skip position |> Seq.take len |> Seq.toList
+            let newAcc = acc @ [add]
+            get_candidates_of_length_acc len lst (position + 1) newAcc
+    get_candidates_of_length_acc length list 0 []
+
 let is_prime x =
     let rec check i =
         x > 1L &&
-       (double i > sqrt (double x) || (x % i <> 0L && check (i + 1L)))
+        (double i > sqrt (double x) || (x % i <> 0L && check (i + 1L)))
     check 2L                
  
 let sequence_of_first_n_primes n =
@@ -27,22 +41,48 @@ let sequence_of_first_n_primes n =
     |> Seq.filter is_prime
     |> Seq.take n
 
+let sequence_of_primes_below n =
+    Seq.initInfinite (fun x -> int64 x)
+    |> Seq.filter is_prime
+    |> Seq.takeWhile (fun elem -> elem < n)
+
 let get_prime_candidate n =
     (sequence_of_first_n_primes n)
     |> Seq.max
 
-let get_next_candidate =
-    let start = 542
+let get_next_candidate start =
     Seq.initInfinite (fun index -> get_prime_candidate (start+index))
 
-let getSolution =
-    let t = (sequence_of_first_n_primes 15 )|> Seq.toList
-    printfn "%A" t
-    let c = get_prime_candidate 542
-    printfn "%i" c
+let has_subsetsum_of_primes_of_length length prime =
+    let primes = 
+        (sequence_of_primes_below 41L)
+        |> Seq.toList
 
-    let c2 = get_next_candidate |> Seq.take 5
-    printfn "%A" c2
+    let candidates = get_candidates_of_length length primes
+    let result =
+        candidates
+        |> List.filter (fun x -> is_sum x prime)
+    result.Length >= 1
+
+let is_valid n =
+    //TODO: Possible to speed up here
+    //let b1 = has_subsetsum_of_primes_of_length 7 n
+    //let b2 = has_subsetsum_of_primes_of_length 17 n
+    //let b3 = has_subsetsum_of_primes_of_length 41 n
+    let b1 = true
+    let b2 = true
+    let b3 = true
+    let b4 = has_subsetsum_of_primes_of_length 541 n
+    printfn "%A" n
+    b1 && b2 && b3 && b4
+
+let getSolution =
+    let b =
+        get_next_candidate 542
+        |> Seq.skipWhile (fun elem -> (is_valid elem) = false)
+        |> Seq.take 1
+    printfn "%A" b
+
     "Not implemented yet"
 
 
