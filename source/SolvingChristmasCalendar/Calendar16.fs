50e5c410ed57c5a0f084d61ccb6fd3e48f9d9c40
Roy Veshovda <roy@veshovda.no>
2014-12-16 09:36:25 +0100

    Refactoring

--- source/SolvingChristmasCalendar/Calendar16.fs
+++ source/SolvingChristmasCalendar/Calendar16.fs
@@ -6,33 +6,14 @@
 //For eksempel, hvis stringen er “02”, så er svaret 10, fordi  2^10 = 1024 og ingen lavere toerpotenser inneholder “02”.
 //Hva er n for den minste toerpotensen som inneholder “472047”? 
 
-//http://stackoverflow.com/questions/383587/how-do-you-do-integer-exponentiation-in-c
-
-//MIN: 687 (SQRT (472047))
-//MAX: Try 2000
-
 open Common
 
 let correct = "1765"
-let expectedRuntimeInMs = 40L
-
-let generate_candidates min max =
-    let numbers = [| min .. max |]
-    numbers |> Array.map (fun elem -> (elem, (System.Numerics.BigInteger.Pow(2I, elem))))
-
-let is_correct (n: bigint) =
-    n.ToString().Contains("472047")
+let expectedRuntimeInMs = 50L
 
 let get_solution =
     let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-
-    let numbers = generate_candidates 687 2000
-    let n2 =
-        numbers
-        |> Array.filter (fun (_, big) -> is_correct big)
-    let (n,_) = n2.[0]
-    
-    let value = sprintf "%A" n
-
+    let number = [1..10000]|>List.find(fun n -> string(bigint 2**n).Contains "472047")
+    let value = sprintf "%A" number
     stopWatch.Stop()
     { ExpectedValue=correct; ActualValue=value; ExpectedRuntimeInMs=expectedRuntimeInMs; ActualRuntimeInMs=stopWatch.ElapsedMilliseconds }
