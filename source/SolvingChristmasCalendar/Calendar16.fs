2d26bb34dc3d9442d5f4538a3391a5d4da5327be
Roy Veshovda <roy@veshovda.no>
2014-12-16 06:29:52 +0100

    Starting 16

--- source/SolvingChristmasCalendar/Calendar16.fs
+++ source/SolvingChristmasCalendar/Calendar16.fs
@@ -6,9 +6,32 @@
 //For eksempel, hvis stringen er “02”, så er svaret 10, fordi  2^10 = 1024 og ingen lavere toerpotenser inneholder “02”.
 //Hva er n for den minste toerpotensen som inneholder “472047”? 
 
+//C#
+//static void Main(string[] args)
+//{
+//    int n = 0;
+//    var running = true;
+//    while (running)
+//    {
+//                
+//        var sum = (BigInteger.Pow(2, n)).ToString();
+//        if (sum.Contains("472047"))
+//        {
+//            running = false;
+//        }
+//        n++;
+//        if (n >= 100000)
+//        {
+//            running = false;
+//        }
+//    }
+//    Console.WriteLine(n-1);
+//    Console.ReadLine();
+//}
+
 open Common
 
-let correct = "N/A"
+let correct = "1765"
 let expectedRuntimeInMs = 0L
 
 let get_solution =
