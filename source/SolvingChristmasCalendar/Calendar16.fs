96ccde9bfa1ed09c894a5f2413f4dce6496caa4f
Roy Veshovda <roy@veshovda.no>
2014-12-16 06:09:04 +0100

    Starting 16

--- source/SolvingChristmasCalendar/Calendar16.fs
+++ source/SolvingChristmasCalendar/Calendar16.fs
@@ -1,7 +1,10 @@
 ﻿module Calendar16
 
 //PROBLEM
-//Not opened yet
+// En toerpotens er et tall som kan skrives som 2^n der n er et heltall.
+//Søk gjennom alle toerpotenser mindre enn 2^10000 og returner første n der en gitt substring forekommer.
+//For eksempel, hvis stringen er “02”, så er svaret 10, fordi  2^10 = 1024 og ingen lavere toerpotenser inneholder “02”.
+//Hva er n for den minste toerpotensen som inneholder “472047”? 
 
 open Common
 
