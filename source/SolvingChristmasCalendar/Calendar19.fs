209cd64ac6807a5d3faa62801f7f6270bad021da
Roy Veshovda <roy@veshovda.no>
2014-12-19 06:45:04 +0100

    Solution for 19

--- source/SolvingChristmasCalendar/Calendar19.fs
+++ source/SolvingChristmasCalendar/Calendar19.fs
@@ -1,15 +1,80 @@
 ﻿module Calendar19
 
 //PROBLEM
-//Not opened yet
+//Et palindrom er et ord som gir samme resultat enten det leses fra høyre eller venstre, for eksempel Lol.
+//Gitt denne stringen på litt over 1200 tegn (separert med linjeskift kun for syns skyld):
+//JegtroringenkanleveetheltlivutenkjærlighetMenkjærlighetenharmange
+//ansikterIhøstkomdetutenboksomheterErlikKjærlighetDenbeståravsamtaler
+//medselgereavgatemagasinetsomnåeretablertimangenorskebyerAlleharde
+//enhistorieåfortelleomkjærlighetsomnoeavgjørendeEntendetertilen
+//partneretfamiliemedlemenvennelleretkjæledyrMangeharopplevdåbli
+//sveketogselvåsvikteMenutrolignokblirikkekjærlighetsevnenødelagt
+//allikevelDenbyggesoppigjengangpågangKjærligheteneretstedåfeste
+//blikketDengirossretningognoeåstyreetterDengirossverdisommennesker
+//ognoeåleveforPåsammemåtesomkjærligheteneretfundamentimenneskelivet
+//erGrunnlovenetfundamentfornasjonenNorgeFor200årsidensamletengruppe
+//mennsegpåEidsvollforålagelovensomskullebligrunnlagettildetselvstendige
+//NorgeGrunnlovensomdengangblevedtattharutvikletsegipaktmedtidenog
+//sikreridagdetnorskefolkrettigheterviletttarforgittihverdagenRettighetersom
+//menneskerimangeandrelandbarekandrømmeomogsomdeslossformedlivet
+//sominnsatsJeghåperatvigjennomjubileumsfeiringeni2014vilbliminnetom
+//hvaGrunnlovenegentligbetyrforosssåvikanfortsetteåarbeideforverdiene
+//vårebådeherhjemmeoginternasjonaltJegharlysttilånevnenoeneksempler
+//påhvordanGrunnlovenvirkerinnpåenkeltmenneskerslivTenkdegatduskriver
+//etkritiskinnleggomlandetsstyrepåsosialemedier
+//Hva er det lengste substrengen du finner i denne strengen som er et palindrom? Svaret skal være palindromet. 
 
 open Common
 
-let correct = "N/A"
-let expectedRuntimeInMs = 0L
+let correct = "etute"
+let expectedRuntimeInMs = 1000L
+
+let text =
+    let t = 
+        "JegtroringenkanleveetheltlivutenkjærlighetMenkjærlighetenharmange"+
+        "ansikterIhøstkomdetutenboksomheterErlikKjærlighetDenbeståravsamtaler"+
+        "medselgereavgatemagasinetsomnåeretablertimangenorskebyerAlleharde"+
+        "enhistorieåfortelleomkjærlighetsomnoeavgjørendeEntendetertilen"+
+        "partneretfamiliemedlemenvennelleretkjæledyrMangeharopplevdåbli"+
+        "sveketogselvåsvikteMenutrolignokblirikkekjærlighetsevnenødelagt"+
+        "allikevelDenbyggesoppigjengangpågangKjærligheteneretstedåfeste"+
+        "blikketDengirossretningognoeåstyreetterDengirossverdisommennesker"+
+        "ognoeåleveforPåsammemåtesomkjærligheteneretfundamentimenneskelivet"+
+        "erGrunnlovenetfundamentfornasjonenNorgeFor200årsidensamletengruppe"+
+        "mennsegpåEidsvollforålagelovensomskullebligrunnlagettildetselvstendige"+
+        "NorgeGrunnlovensomdengangblevedtattharutvikletsegipaktmedtidenog"+
+        "sikreridagdetnorskefolkrettigheterviletttarforgittihverdagenRettighetersom"+
+        "menneskerimangeandrelandbarekandrømmeomogsomdeslossformedlivet"+
+        "sominnsatsJeghåperatvigjennomjubileumsfeiringeni2014vilbliminnetom"+
+        "hvaGrunnlovenegentligbetyrforosssåvikanfortsetteåarbeideforverdiene"+
+        "vårebådeherhjemmeoginternasjonaltJegharlysttilånevnenoeneksempler"+
+        "påhvordanGrunnlovenvirkerinnpåenkeltmenneskerslivTenkdegatduskriver"+
+        "etkritiskinnleggomlandetsstyrepåsosialemedier"
+    t.ToLower()
+
+let is_palindrome (s: string) =
+   let arr = s.ToCharArray()
+   arr = Array.rev arr
+
+let get_sub_palindromes (text: string)=
+    let length = text.Length
+    [|
+        for c in 0 .. length do
+            let stop = length - c
+            for i in 1 .. stop do
+                let s = text.Substring(c,i)
+                if (is_palindrome s) then yield s
+    |]
 
 let get_solution =
     let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-    //TODO: Implement here
+    let value =
+        get_sub_palindromes (text.ToLower())
+        |> Array.maxBy (fun (elem:string) -> elem.Length)
     stopWatch.Stop()
-    { ExpectedValue=correct; ActualValue=correct; ExpectedRuntimeInMs=expectedRuntimeInMs; ActualRuntimeInMs=stopWatch.ElapsedMilliseconds }
+    {
+        ExpectedValue=correct;
+        ActualValue=value;
+        ExpectedRuntimeInMs=expectedRuntimeInMs;
+        ActualRuntimeInMs=stopWatch.ElapsedMilliseconds
+    }
