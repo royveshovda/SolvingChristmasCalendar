53161101a9d6d0f5102406cd738b30879ec8710c
Roy Veshovda <roy@veshovda.no>
2014-12-24 07:26:35 +0100

    Improve 19

--- source/SolvingChristmasCalendar/Calendar19.fs
+++ source/SolvingChristmasCalendar/Calendar19.fs
@@ -9,7 +9,7 @@
 open Common
 
 let correct = "etute"
-let expectedRuntimeInMs = 1000L
+let expectedRuntimeInMs = 920L
 
 let text =
     let t = 
@@ -36,19 +36,19 @@ let text =
 
 let get_sub_palindromes (text: string)=
     let length = text.Length
-    [|
+    seq {
         for c in 0 .. length do
             let stop = length - c
             for i in 1 .. stop do
                 let s = text.Substring(c,i)
                 if (is_palindrome s) then yield s
-    |]
+    }
 
 let get_solution =
     let stopWatch = System.Diagnostics.Stopwatch.StartNew()
     let value =
         get_sub_palindromes (text.ToLower())
-        |> Array.maxBy (fun (elem:string) -> elem.Length)
+        |> Seq.maxBy (fun (elem:string) -> elem.Length)
     stopWatch.Stop()
     {
         ExpectedValue=correct;
